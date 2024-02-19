8a11b03009bc6799082f2f10293f82e9d02e7715
Arnold Maala <a.maala@groupmfi.com>
2024-02-19 16:33:04 +0300

    Switched from hardcoded quotes to fetching from an API

--- generator.js
+++ generator.js
@@ -1,4 +1,4 @@
-let quotes = [
+/*let quotes = [
     "The only thing that matters is that you have a good life.",
     "Life is what happens to you while you're busy making other plans.",
     "When music hits you, you feel no pain.",
@@ -16,11 +16,39 @@ let authors = [
     "Yogi Berra",
     "Abraham Lincoln",
     "Johann Wolfgang von Goethe"
-];
+];*/
+
+const quoteText = document.querySelector(".quote");
+const quoteAuthor = document.querySelector(".author");
+quoteBtn = document.querySelector(".button");
+soundBtn = document.querySelector(".sound");
+
+const apiUrl = 'https://api.quotable.io/random';
 
 function randomQuote() {
-    let randomIndex = Math.floor(Math.random() * quotes.length);
+    quoteBtn.innerText = "Loading Quote...";
+    // Fetching data from the API and parsing it into json objects
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(result => {
+            quoteText.innerText = result.content;
+            quoteAuthor.innerText = "- " + result.author;
+
+            quoteBtn.innerText = "New Quote";
+    });
+
+    //let randomIndex = Math.floor(Math.random() * quotes.length);
     
-    document.querySelector('.quote').innerHTML = quotes[randomIndex];
-    document.querySelector('.author').innerHTML = "- " + authors[randomIndex];
+    //document.querySelector('.quote').innerHTML = quotes[randomIndex];
+    //document.querySelector('.author').innerHTML = "- " + authors[randomIndex];
 }
+
+randomQuote();
+soundBtn.addEventListener('click', () => {
+    // Web speech API that represents a speech request
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${quoteAuthor.innerText}`);
+
+    speechSynthesis.speak(utterance);
+    console.log(utterance);
+});
+quoteBtn.addEventListener("click", randomQuote);
